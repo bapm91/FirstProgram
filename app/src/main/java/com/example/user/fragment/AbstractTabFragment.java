207c3dd608afebc80ca0cac7da6593bc5a21c1b9
bapm91 <Kulynych.v.a@gmail.com>
2016-04-22 20:11:05 +0300

    Add fragment classes

---
+++ app/src/main/java/com/example/user/fragment/AbstractTabFragment.java
@@ -0,0 +1,21 @@
+package com.example.user.fragment;
+
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public class AbstractTabFragment extends Fragment {
+
+    protected String title;
+    protected Context context;
+    protected View mView;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
